fa647b2ec21d43d3f010669544bd112ba0cce8b1
wklausing <62486651+wklausing@users.noreply.github.com>
2023-01-13 20:26:41 +0100

    Create README.md and small changes like adding some prints.

--- 13_lines_example.py
+++ 13_lines_example.py
@@ -3,6 +3,7 @@ from stable_baselines3 import PPO
 from pettingzoo.butterfly import pistonball_v4
 import supersuit as ss
 
+print("Starting")
 env = pistonball_v4.parallel_env(n_pistons=20, time_penalty=-0.1, continuous=True, random_drop=True, random_rotate=True, ball_mass=0.75, ball_friction=0.3, ball_elasticity=1.5, max_cycles=125)
 # env = ss.color_reduction_v0(env, mode='B')
 # env = ss.resize_v0(env, x_size=84, y_size=84)
@@ -10,16 +11,16 @@ env = pistonball_v4.parallel_env(n_pistons=20, time_penalty=-0.1, continuous=Tru
 env = ss.pettingzoo_env_to_vec_env_v0(env)
 env = ss.concat_vec_envs_v0(env, 8, num_cpus=4, base_class='stable_baselines3')
 
-model = PPO(CnnPolicy, env, verbose=3, gamma=0.95, n_steps=2, ent_coef=0.0905168, learning_rate=0.00062211, vf_coef=0.042202, max_grad_norm=0.9, gae_lambda=0.99, n_epochs=5, clip_range=0.3, batch_size=256)
+model = PPO(CnnPolicy, env, verbose=3, gamma=0.95, n_steps=256, ent_coef=0.0905168, learning_rate=0.00062211, vf_coef=0.042202, max_grad_norm=0.9, gae_lambda=0.99, n_epochs=5, clip_range=0.3, batch_size=256)
 
 print("Train")
-model.learn(total_timesteps=5)
+model.learn(total_timesteps=100)
 
 print("Save policy")
 model.save("policy")
-print("Done")
 
 
+print("Rendering")
 env = pistonball_v4.env()
 model = PPO.load("policy")
 env.reset()
@@ -28,3 +29,4 @@ for agent in env.agent_iter():
     act = model.predict(obs, deterministic=True)[0] if not done else None
     env.step(act)
     env.render()
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# pettingZoo_13_lines_example
+PettingZoo 13 lines example from https://towardsdatascience.com/multi-agent-deep-reinforcement-learning-in-15-lines-of-code-using-pettingzoo-e0b963c0820b
+
+Modified to get it running.
+
+Python == 3.7.16
+pip == 22.0.4
